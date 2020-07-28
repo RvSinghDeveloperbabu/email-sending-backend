96b51a40bf3f8d81ad09309f8cd568b166fe0256
ravi kumar <ravikumar.developerbabu@gmail.com>
2020-07-28 22:08:35 +0530

    configer mongodb cloud service and created user recode init

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express  = require('express');
-require("./services/passport");
 const app = express();
+const keys = require('./config/keys')
 require('./routes/authRoutes')(app);
+// models
+require('./models/User')
 
+// passport
+require("./services/passport");
+
+const mongoose = require('mongoose')
+
+mongoose.connect(keys.mongodbURI,
+    {useUnifiedTopology: true });
+
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://emaily_dev:<password>@testingnodereactapp.5lj4w.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//     const collection = client.db("test").collection("devices");
+//     // perform actions on the collection object
+//     client.close();
+// });
 
 const PORT = process.env.PORT || 3001;
 
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport  = require('passport')
 const keys = require('../config/keys')
 const GoogleStrategy =require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+
+const  User = mongoose.model('users');
 
 passport.use(
     new GoogleStrategy({
@@ -8,7 +11,7 @@ passport.use(
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
     },(accessToken, refreshToken, profile, done)=>{
-        debugger
+       new User({googleId: profile.id}).save();
         console.log("accessToken ==>",accessToken);
         console.log("refresh Token ==>",refreshToken);
         console.log("Profile ==>",profile);
