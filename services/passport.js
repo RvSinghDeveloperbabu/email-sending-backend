f64988a1f12ea0cf85f34afeece14ce107a8fca2
ravi kumar <ravikumar.developerbabu@gmail.com>
2020-07-29 20:20:03 +0530

    checking existing user and completig passport auth and send user back.

--- services/passport.js
+++ services/passport.js
@@ -11,7 +11,17 @@ passport.use(
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
     },(accessToken, refreshToken, profile, done)=>{
-       new User({googleId: profile.id}).save();
+        User.findOne({googleId: profile.id})
+            .then((existingUser)=>{
+                if (existingUser){
+                    // we don't create any user into mongoDB
+                    done(null,existingUser)
+                }else {
+                    // we create any user into mongoDB
+                    new User({googleId: profile.id}).save()
+                    .then(newUser => done(null,newUser));
+                }
+            })
         console.log("accessToken ==>",accessToken);
         console.log("refresh Token ==>",refreshToken);
         console.log("Profile ==>",profile);
