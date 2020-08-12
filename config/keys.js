4a549ae2bd03def974377fc2ac04416d72bb1223
ravi kumar <ravikumar.developerbabu@gmail.com>
2020-08-12 19:59:31 +0530

    adding differnt environment keys for server

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+
+if (process.env.NODE_ENV === 'production'){
+    // use production keys
+}else{
+    // use development keys
+}
+
+// cIDgoogle= 298894517177-p93f11ok59irp55pjnfrqu470ctsltuo.apps.googleusercontent.com
+// secretgoogle= DmCBJnAO-Qb6flPJQJ7Q3ZE-
