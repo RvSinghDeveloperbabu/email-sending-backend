eef4475a8214b18ae46b974c71f19a89f760afa5
ravi kumar <ravikumar.developerbabu@gmail.com>
2020-08-12 20:10:30 +0530

    adding production keys

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,10 @@
 
 if (process.env.NODE_ENV === 'production'){
     // use production keys
+    module.exports = require('./production_keys')
 }else{
     // use development keys
+    module.exports =require('./dev')
 }
 
 // cIDgoogle= 298894517177-p93f11ok59irp55pjnfrqu470ctsltuo.apps.googleusercontent.com
